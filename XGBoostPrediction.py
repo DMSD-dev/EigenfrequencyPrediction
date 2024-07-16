1a247bf4caed172ac39550754984f8946f3852b1
DMSD-dev <144464901+DMSD-dev@users.noreply.github.com>
2024-07-17 00:22:55 +0200

    Update XGBoostPrediction.py
    changed variable-names

--- XGBoostPrediction.py
+++ XGBoostPrediction.py
@@ -15,7 +15,7 @@ Notes:
 - Data is expected in the following formats:
     - Features: 2D numpy array of datapoints in first dimension and second dimensions representing, in order, the following metrics:
     
-    n0_3,STW_NUM,BIEGESTEIFIGKEIT,MASSE,BAUJAHR,V_SOLL_IST,BETA_HT_M_DEB,HOEHE_KONST_UEB_MITTE_DEB,Ssp_Randkonstruktion,WANDSCHIEFE_RAHMEN_P1_NUM2,DEB_I_LAGERUNG_UEBERBAU2
+    eigenfrequency_n03_Hz,span_L_m,stiffness_EI_MNm2,mass_m_tpm,yoc_yyyy,maxlocalspeed_v_kmph,bearingcapacity_beta_,heigtogconstruction_hk_m,numberofcaps_,skewness_degree,bearing_
 
     - Labels: 2D numpy array of the measured eigenfrequency in the second dimension for each datapoint represented in the first dimension
     - Both must, row-wise, correspond to the same datapoint, and must thus in particular match in the first dimension
